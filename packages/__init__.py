e91989ca328a6f14559f03c0974a08ff67f97c32
Ambati Sathwik <75169261+AmbatiSathwik@users.noreply.github.com>
2021-11-25 05:59:15 -0500

    Add files via upload

---
+++ packages/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+# from .auth import auth
+# from . import db
+import urllib.parse as urlparse
+import os
+
+# URL =  urlparse.urlparse(os.environ['DATABASE_URL'])
+# DEBUG = True
+# SECRET_KEY = 'sathwikisgreat'
+
+# TEMPLATE_DIR = os.path.abspath('templates')
+
+def create_app():
+    app = Flask(__name__, static_folder='static')
+    app.config.from_object(__name__)
+    # app.register_blueprint(auth, url_prefix='/')
+    # db.init_app(app)
+    return app
---
+++ packages/sql/000_create.sql
@@ -0,0 +1,6 @@
+drop table user if exists;
+drop table room if exists;
+drop table hostel if exists;
+drop table residents if exists;
+drop table complients if exists;
+
