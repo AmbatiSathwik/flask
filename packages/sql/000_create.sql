d54d1c1400a3f0d027f6f1027cb4dfd804d1b6bd
AmbatiSathwik <ambatisathwil2311@gmail.com>
2021-11-26 02:02:30 -0500

    sql commands written

--- packages/sql/000_create.sql
+++ packages/sql/000_create.sql
@@ -1,6 +1,47 @@
-drop table user if exists;
-drop table room if exists;
-drop table hostel if exists;
-drop table residents if exists;
-drop table complients if exists;
+drop table if exists usr;
+drop table if exists room;
+drop table if exists hostel;
+drop table if exists residents;
+drop table if exists complients;
 
+CREATE TABLE usr(
+    id VARCHAR(20) PRIMARY KEY,
+    name TEXT,
+    email TEXT,
+    phone VARCHAR(20),
+    password VARCHAR(20),
+    is_admin BOOLEAN
+);
+
+CREATE TABLE hostel(
+    id VARCHAR(10) PRIMARY KEY,
+    Hname TEXT,
+    Nroom INTEGER
+);
+
+CREATE TABLE room(
+    id VARCHAR(10) PRIMARY KEY,
+    vacancy INTEGER,
+    capacity INTEGER,
+    hid VARCHAR(10),
+    FOREIGN KEY (hid) REFERENCES hostel(id)
+);
+
+CREATE TABLE resident(
+    sid VARCHAR(20),
+    rid VARCHAR(10),
+    FOREIGN KEY (sid) REFERENCES usr(id),
+    FOREIGN KEY (rid) REFERENCES room(id)
+);
+
+CREATE TABLE complient(
+    Cno SERIAL PRIMARY KEY,
+    Ctype VARCHAR(20),
+    description TEXT,
+    sid VARCHAR(20),
+    rid VARCHAR(10),
+    hid VARCHAR(10),
+    FOREIGN KEY (hid) REFERENCES hostel(id),
+    FOREIGN KEY (sid) REFERENCES usr(id),
+    FOREIGN KEY (rid) REFERENCES room(id)
+);
